3457946b2730e3628ee4c6fc98dbf8021c03848f
Lenh Ha Nhat <hanhatlenh@Lenhs-MacBook-Pro.local>
2017-07-24 15:38:54 +0900

    Commited from hanhatlenh at Mon Jul 24 15:38:54 JST 2017

---
+++ modules/workdates/client/config/workdates.client.config.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('workdates')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Workdates',
+      state: 'workdates',
+      type: 'dropdown',
+      roles: ['*']
+    });
+
+    // Add the dropdown list item
+    menuService.addSubMenuItem('topbar', 'workdates', {
+      title: 'List Workdates',
+      state: 'workdates.list'
+    });
+
+    // Add the dropdown create item
+    menuService.addSubMenuItem('topbar', 'workdates', {
+      title: 'Create Workdate',
+      state: 'workdates.create',
+      roles: ['user']
+    });
+  }
+}());
