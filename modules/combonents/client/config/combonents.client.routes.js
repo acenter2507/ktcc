062d0a585f7d1db32a67f508bc31b6e315f01919
Ha Nhat Lenh <lenh@ktc.local>
2017-07-25 16:36:24 +0900

    Commited from lenh at Tue, Jul 25, 2017  4:36:24 PM

--- modules/combonents/client/config/combonents.client.routes.js
+++ modules/combonents/client/config/combonents.client.routes.js
@@ -16,43 +16,43 @@
       })
       .state('combonents.buttons', {
         url: '/buttons',
-        templateUrl: 'modules/combonents/client/views/buttons.client.view.html'
+        template: '<h1>buttons</h1>'
       })
       .state('combonents.social', {
         url: '/social',
-        templateUrl: 'modules/combonents/client/views/social.client.view.html'
+        template: '<h1>social</h1>'
       })
       .state('combonents.cards', {
         url: '/cards',
-        templateUrl: 'modules/combonents/client/views/cards.client.view.html'
+        template: '<h1>cards</h1>'
       })
       .state('combonents.forms', {
         url: '/forms',
-        templateUrl: 'modules/combonents/client/views/forms.client.view.html'
+        template: '<h1>forms</h1>'
       })
       .state('combonents.switches', {
         url: '/switches',
-        templateUrl: 'modules/combonents/client/views/switches.client.view.html'
+        template: '<h1>switches</h1>'
       })
       .state('combonents.tables', {
         url: '/tables',
-        templateUrl: 'modules/combonents/client/views/tables.client.view.html'
+        template: '<h1>tables</h1>'
       })
       .state('combonents.login', {
         url: '/login',
-        templateUrl: 'modules/combonents/client/views/login.client.view.html'
+        template: '<h1>login</h1>'
       })
       .state('combonents.register', {
         url: '/register',
-        templateUrl: 'modules/combonents/client/views/register.client.view.html'
+        template: '<h1>register</h1>'
       })
       .state('combonents.404', {
         url: '/404',
-        templateUrl: 'modules/combonents/client/views/404.client.view.html'
+        template: '<h1>404</h1>'
       })
       .state('combonents.500', {
         url: '/500',
-        templateUrl: 'modules/combonents/client/views/500.client.view.html'
+        template: '<h1>500</h1>'
       });
   }
 }());
