d3ecf3ac8bc48d94a57908d102c2ccd94bc90159
Ha Nhat Lenh <lenh@ktc.local>
2017-07-25 16:29:10 +0900

    Commited from lenh at Tue, Jul 25, 2017  4:29:10 PM

--- modules/combonents/client/config/combonents.client.routes.js
+++ modules/combonents/client/config/combonents.client.routes.js
@@ -16,73 +16,43 @@
       })
       .state('combonents.buttons', {
         url: '/buttons',
-        templateUrl: 'modules/combonents/client/views/buttons.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Buttons'
-        }
+        templateUrl: 'modules/combonents/client/views/buttons.client.view.html'
       })
       .state('combonents.social', {
         url: '/social',
-        templateUrl: 'modules/combonents/client/views/social.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Social Buttons'
-        }
+        templateUrl: 'modules/combonents/client/views/social.client.view.html'
       })
       .state('combonents.cards', {
         url: '/cards',
-        templateUrl: 'modules/combonents/client/views/cards.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Cards'
-        }
+        templateUrl: 'modules/combonents/client/views/cards.client.view.html'
       })
       .state('combonents.forms', {
         url: '/forms',
-        templateUrl: 'modules/combonents/client/views/forms.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Forms'
-        }
+        templateUrl: 'modules/combonents/client/views/forms.client.view.html'
       })
       .state('combonents.switches', {
         url: '/switches',
-        templateUrl: 'modules/combonents/client/views/switches.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Switches'
-        }
+        templateUrl: 'modules/combonents/client/views/switches.client.view.html'
       })
       .state('combonents.tables', {
         url: '/tables',
-        templateUrl: 'modules/combonents/client/views/tables.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Tables'
-        }
+        templateUrl: 'modules/combonents/client/views/tables.client.view.html'
       })
       .state('combonents.login', {
         url: '/login',
-        templateUrl: 'modules/combonents/client/views/login.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Login'
-        }
+        templateUrl: 'modules/combonents/client/views/login.client.view.html'
       })
       .state('combonents.register', {
         url: '/register',
-        templateUrl: 'modules/combonents/client/views/register.client.view.html',
-        ncyBreadcrumb: {
-          label: 'Register'
-        }
+        templateUrl: 'modules/combonents/client/views/register.client.view.html'
       })
       .state('combonents.404', {
         url: '/404',
-        templateUrl: 'modules/combonents/client/views/404.client.view.html',
-        ncyBreadcrumb: {
-          label: '404'
-        }
+        templateUrl: 'modules/combonents/client/views/404.client.view.html'
       })
       .state('combonents.500', {
         url: '/500',
-        templateUrl: 'modules/combonents/client/views/500.client.view.html',
-        ncyBreadcrumb: {
-          label: '500'
-        }
+        templateUrl: 'modules/combonents/client/views/500.client.view.html'
       });
   }
 }());
