298318bfa4cf0f0496db1de89d9be85ea0c0604b
Ha Nhat Lenh <lenh@ktc.local>
2017-07-27 11:52:22 +0900

    Commited from lenh at Thu, Jul 27, 2017 11:52:22 AM

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -9,12 +9,10 @@
 
   function HeaderController($scope, $state, Authentication, menuService) {
     var vm = this;
-    vm.user = vm.authentication.user;
-    vm.isLogged = (vm.authentication.user);
+    vm.user = Authentication.user;
+    vm.isLogged = (vm.user);
 
     vm.accountMenu = menuService.getMenu('account').items[0];
-    vm.authentication = Authentication;
-    vm.user = vm.authentication.user;
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
 
