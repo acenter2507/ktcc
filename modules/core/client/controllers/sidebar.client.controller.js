0f4852729a1c34019c00c71a169de92bcfb8e47d
Ha Nhat Lenh <lenh@ktc.local>
2017-07-27 11:35:23 +0900

    Commited from lenh at Thu, Jul 27, 2017 11:35:23 AM

--- modules/core/client/controllers/sidebar.client.controller.js
+++ modules/core/client/controllers/sidebar.client.controller.js
@@ -10,6 +10,6 @@
   function SidebarController($scope, $state, Authentication, menuService) {
     var vm = this;
     vm.user = Authentication.user;
-    vm.isLogged = (vm.user) ? true : false;
+    vm.isLogged = (vm.user);
   }
 }());
