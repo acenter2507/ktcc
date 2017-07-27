fadd5489d7b76a2ea5f1068aecc840231d45e11d
Ha Nhat Lenh <lenh@ktc.local>
2017-07-27 17:11:26 +0900

    Commited from lenh at Thu, Jul 27, 2017  5:11:25 PM

--- modules/workdates/client/config/workdates.client.routes.js
+++ modules/workdates/client/config/workdates.client.routes.js
@@ -16,7 +16,7 @@
       })
       .state('workdates.list', {
         url: '',
-        templateUrl: 'modules/workdates/client/views/list-workdates.client.view.html',
+        templateUrl: '/modules/workdates/client/views/list-workdates.client.view.html',
         controller: 'WorkdatesListController',
         controllerAs: 'vm',
         data: {
@@ -25,7 +25,7 @@
       })
       .state('workdates.create', {
         url: '/create',
-        templateUrl: 'modules/workdates/client/views/form-workdate.client.view.html',
+        templateUrl: '/modules/workdates/client/views/form-workdate.client.view.html',
         controller: 'WorkdatesController',
         controllerAs: 'vm',
         resolve: {
@@ -38,7 +38,7 @@
       })
       .state('workdates.edit', {
         url: '/:workdateId/edit',
-        templateUrl: 'modules/workdates/client/views/form-workdate.client.view.html',
+        templateUrl: '/modules/workdates/client/views/form-workdate.client.view.html',
         controller: 'WorkdatesController',
         controllerAs: 'vm',
         resolve: {
@@ -51,7 +51,7 @@
       })
       .state('workdates.view', {
         url: '/:workdateId',
-        templateUrl: 'modules/workdates/client/views/view-workdate.client.view.html',
+        templateUrl: '/modules/workdates/client/views/view-workdate.client.view.html',
         controller: 'WorkdatesController',
         controllerAs: 'vm',
         resolve: {
