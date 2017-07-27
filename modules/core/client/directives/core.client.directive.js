e7cabd6157bd2bcefd03a831ae7d1c2e4c3ae339
Ha Nhat Lenh <lenh@ktc.local>
2017-07-27 09:37:32 +0900

    Commited from lenh at Thu, Jul 27, 2017  9:37:31 AM

--- modules/core/client/directives/core.client.directive.js
+++ modules/core/client/directives/core.client.directive.js
@@ -121,11 +121,11 @@ function layoutToggleDirective($interval) {
     element.on('click', function () {
 
       if (element.hasClass('sidebar-toggler')) {
-        //angular.element('body').toggleClass('sidebar-hidden');
+        angular.element('body').toggleClass('sidebar-hidden');
       }
 
       if (element.hasClass('aside-menu-toggler')) {
-        //angular.element('body').toggleClass('aside-menu-hidden');
+        angular.element('body').toggleClass('aside-menu-hidden');
       }
     });
   }
