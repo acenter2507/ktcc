ead0458ff5f0c31720772ba61f86dc06d6d78ca0
Ha Nhat Lenh <lenh@ktc.local>
2017-07-28 11:13:58 +0900

    Commited from lenh at Fri, Jul 28, 2017 11:13:58 AM

--- modules/workdates/client/controllers/list-workdates.client.controller.js
+++ modules/workdates/client/controllers/list-workdates.client.controller.js
@@ -11,7 +11,7 @@
     var vm = this;
     vm.user = Authentication.user;
     vm.isLogged = (vm.user);
-    vm.currentMonth = moment(new Date(), 'YYYY/MM').month(2);
+    vm.currentMonth = moment(new Date(), 'YYYY/MM');
 
     vm.startDate = moment(vm.currentMonth).subtract(1, 'months').date(21);
     vm.endDate = moment(vm.currentMonth).date(20);
@@ -19,18 +19,19 @@
     vm.daysOfMonth = [];
     init_daysOfMonth();
     function init_daysOfMonth() {
-      // var startDate = vm.startDate.clone().startOf('day');
-      // var lastDate = vm.endDate.clone().startOf('day');
+      var startDate = vm.startDate.clone().startOf('day');
+      var lastDate = vm.endDate.clone().startOf('day');
 
-      vm.daysOfMonth.push(vm.startDate.clone());
-      while (vm.startDate.clone().add(1, 'days').diff(vm.endDate) < 0) {
-        vm.daysOfMonth.push(vm.startDate.clone());
+      vm.daysOfMonth.push(startDate.clone());
+      while (startDate.add(1, 'days').diff(lastDate) < 0) {
+        console.log(startDate);
+        vm.daysOfMonth.push(startDate.clone());
       }
-      vm.daysOfMonth.push(vm.endDate.clone());
+      vm.daysOfMonth.push(lastDate.clone());
     }
 
 
-    console.log(vm.daysOfMonth);
+    // console.log(vm.startDate.format('LLLL'));
     // console.log(vm.endDate.format('LLLL'));
     // console.log(vm.currentMonth.startOf('month').format('LLLL'));
     // console.log(vm.currentMonth.endOf('month').format('LLLL'));
