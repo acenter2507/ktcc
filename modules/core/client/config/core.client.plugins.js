04fde18ca43680973874c6b573de44f47367e541
Ha Nhat Lenh <lenh@ktc.local>
2017-07-28 10:17:29 +0900

    Commited from lenh at Fri, Jul 28, 2017 10:17:29 AM

--- modules/core/client/config/core.client.plugins.js
+++ modules/core/client/config/core.client.plugins.js
@@ -33,6 +33,10 @@
   }
 
   function runConfig(amMoment) {
+    moment.tz.add([
+      'Asia/Tokyo|JST JDT|-90 -a0|010101010|-QJH0 QL0 1lB0 13X0 1zB0 NX0 1zB0 NX0|38e6',
+      'Asia/Ho_Chi_Minh|LMT PLMT +07 +08 +09|-76.E -76.u -70 -80 -90|0123423232|-2yC76.E bK00.a 1h7b6.u 5lz0 18o0 3Oq0 k5b0 aW00 BAM0|90e5'
+    ]);
     moment.locale('ja');
     amMoment.changeLocale('ja');
   }
