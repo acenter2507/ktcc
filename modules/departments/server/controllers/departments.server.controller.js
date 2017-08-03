15628f2d6be98de4f8ff20b5e1006358bc3d830a
Ha Nhat Lenh <lenh@ktc.local>
2017-08-03 18:06:56 +0900

    Commited from lenh at Thu, Aug 03, 2017  6:06:56 PM

--- modules/departments/server/controllers/departments.server.controller.js
+++ modules/departments/server/controllers/departments.server.controller.js
@@ -6,6 +6,7 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Department = mongoose.model('Department'),
+  User = mongoose.model('User'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
   _ = require('lodash');
 
