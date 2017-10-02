bb4426472c69378b2151f65a9ad314665f2437ad
Ha Nhat Lenh <lenh@ktc.local>
2017-10-02 15:44:04 +0900

    Commited from lenh at Mon, Oct 02, 2017  3:44:03 PM

--- modules/months/server/models/month.server.model.js
+++ modules/months/server/models/month.server.model.js
@@ -12,7 +12,7 @@ var mongoose = require('mongoose'),
 var MonthSchema = new Schema({
   month: { type: Number, required: true },
   year: { type: Number, required: true },
-  status: { type: Number, default: 1 },// Unsend, Watting, Approve, Reject, Ok
+  status: { type: Number, default: 1 }, // Unsend, Watting, Approve, Reject, Ok
   comment: { type: String, default: '' }, // レビューする人からのコメント
   worked: { type: Number, default: 0 }, // 出勤した日数
   created: { type: Date, default: Date.now },
--- modules/workdates/server/models/workdate.server.model.js
+++ modules/workdates/server/models/workdate.server.model.js
@@ -18,11 +18,11 @@ var WorkdateSchema = new Schema({
   start: { type: String, default: '' }, // 開始
   end: { type: String, default: '' }, // 終了
   middleRest: { type: Number, default: 1 }, // 休憩 (hour)
-  overtime: { type: Number, default: 0 },// 時間外
-  late: { type: Number, default: 0 },// 深夜
+  overtime: { type: Number, default: 0 }, // 時間外
+  late: { type: Number, default: 0 }, // 深夜
   comment: { type: String, trim: true }, // Approve, reject, waiting
-  restmode: { type: Schema.ObjectId, ref: 'Restmode' },// 休日形態
-  transfer: { type: Boolean, default: false },// 振替
+  restmode: { type: Schema.ObjectId, ref: 'Restmode' }, // 休日形態
+  transfer: { type: Boolean, default: false }, // 振替
   created: { type: Date, default: Date.now },
   updated: { type: Date, default: Date.now },
   user: { type: Schema.ObjectId, ref: 'User' }
