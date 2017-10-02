8b8f974ae9471167212ee6d2c16bc859f3982f85
Ha Nhat Lenh <lenh@ktc.local>
2017-10-02 17:36:56 +0900

    Commited from lenh at Mon, Oct 02, 2017  5:36:56 PM

--- modules/users/client/controllers/authentication.client.controller.js
+++ modules/users/client/controllers/authentication.client.controller.js
@@ -5,15 +5,13 @@
     .module('users')
     .controller('AuthenticationController', AuthenticationController);
 
-  AuthenticationController.$inject = ['$rootScope', '$scope', '$state', 'UsersService', '$location', '$window', 'Authentication', 'PasswordValidator', 'Notification'];
+  AuthenticationController.$inject = ['$rootScope', '$scope', '$state', 'UsersService', '$location', '$window', 'Authentication', 'Notification'];
 
-  function AuthenticationController($rootScope, $scope, $state, UsersService, $location, $window, Authentication, PasswordValidator, Notification) {
+  function AuthenticationController($rootScope, $scope, $state, UsersService, $location, $window, Authentication, Notification) {
     var vm = this;
 
-    vm.authentication = Authentication;
     vm.signup = signup;
     vm.signin = signin;
-    vm.callOauthProvider = callOauthProvider;
     vm.usernameRegex = /^(?=[\w.-]+$)(?!.*[._-]{2})(?!\.)(?!.*\.$).{3,34}$/;
 
     // Get an eventual error defined in the URL query string:
@@ -22,21 +20,8 @@
     }
 
     // If user is signed in then redirect back home
-    if (vm.authentication.user) {
+    if ($scope.isLogged) {
       $location.path('/');
-    }
-
-    function signup(isValid) {
-
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'vm.userForm');
-
-        return false;
-      }
-
-      UsersService.userSignup(vm.credentials)
-        .then(onUserSignupSuccess)
-        .catch(onUserSignupError);
     }
 
     function signin(isValid) {
@@ -51,36 +36,11 @@
         .catch(onUserSigninError);
     }
 
-    // OAuth provider request
-    function callOauthProvider(url) {
-      if ($state.previous && $state.previous.href) {
-        url += '?redirect_to=' + encodeURIComponent($state.previous.href);
-      }
-      // Effectively call OAuth authentication route:
-      $window.location.href = url;
-    }
-
-    // Authentication Callbacks
-
-    function onUserSignupSuccess(response) {
-      // If successful we assign the response to the global user model
-      vm.authentication.user = response;
-      Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Signup successful!' });
-      // And redirect to the previous or home page
-      $rootScope.$emit('loginSuccess');
-      $state.go($state.previous.state.name || 'home', $state.previous.params);
-    }
-
-    function onUserSignupError(response) {
-      Notification.error({ message: response.data.message, title: '<i class="glyphicon glyphicon-remove"></i> Signup Error!', delay: 6000 });
-    }
-
     function onUserSigninSuccess(response) {
       // If successful we assign the response to the global user model
-      vm.authentication.user = response;
+      Authentication.user = response;
       Notification.info({ message: 'Welcome ' + response.firstName });
       // And redirect to the previous or home page
-      $rootScope.$emit('loginSuccess');
       $state.go($state.previous.state.name || 'home', $state.previous.params);
     }
 
