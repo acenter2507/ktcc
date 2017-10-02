31985a5a25b920447bcf4b61e26454ec6ac5de5c
Ha Nhat Lenh <lenh@ktc.local>
2017-10-02 20:34:10 +0900

    Commited from lenh at Mon, Oct 02, 2017  8:34:09 PM

--- modules/users/client/controllers/admin/list-users.client.controller.js
+++ modules/users/client/controllers/admin/list-users.client.controller.js
@@ -5,10 +5,11 @@
     .module('users')
     .controller('UserListController', UserListController);
 
-  UserListController.$inject = ['$scope', '$filter', 'AdminUserService', 'AdminUserApi'];
+  UserListController.$inject = ['$scope', '$filter', 'AdminUserService', 'AdminUserApi', 'DepartmentsService'];
 
-  function UserListController($scope, $filter, AdminUserService, AdminUserApi) {
+  function UserListController($scope, $filter, AdminUserService, AdminUserApi, DepartmentsService) {
     var vm = this;
+    vm.departments = DepartmentsService.query();
     vm.users = [];
     vm.condition = {};
     vm.page = 1;
@@ -34,7 +35,15 @@
       vm.page = page;
       handleLoadUsers();
     };
-
+    vm.handleSearch = () => {
+      vm.page = 1;
+      handleLoadUsers();
+    };
+    vm.handleClearCondition = () => {
+      vm.condition = {};
+      vm.page = 1;
+      handleLoadUsers();
+    };
     function createArrayFromRange(range) {
       var array = [];
       for (var i = 1; i <= range; i++) {
