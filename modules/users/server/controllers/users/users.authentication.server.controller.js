4bed6f7b6d3c0282ca3c1a03b5612540f2560b76
Ha Nhat Lenh <lenh@ktc.local>
2017-10-02 19:08:49 +0900

    Commited from lenh at Mon, Oct 02, 2017  7:08:49 PM

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -20,6 +20,9 @@ var noReturnUrls = [
  */
 exports.signin = function (req, res, next) {
   passport.authenticate('local', function (err, user, info) {
+    console.log(err);
+    console.log(user);
+    console.log(info);
     if (err || !user) {
       res.status(422).send(info);
     } else {
