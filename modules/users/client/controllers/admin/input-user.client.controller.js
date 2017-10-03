f32c20552aa6feb0c4d1aa6cebfe73f58bddc201
Ha Nhat Lenh <lenh@ktc.local>
2017-10-03 09:52:15 +0900

    Commited from lenh at Tue, Oct 03, 2017  9:52:15 AM

--- modules/users/client/controllers/admin/input-user.client.controller.js
+++ modules/users/client/controllers/admin/input-user.client.controller.js
@@ -19,47 +19,12 @@
         vm.user.department = vm.user.department._id || vm.user.department;
       }
       vm.dateOptions = {
-        dateDisabled: disabled,
+        dateDisabled: false,
         maxDate: new Date(),
         minDate: new Date(1900, 1, 1),
         startingDay: 1
       };
       vm.openDatePicker = false;
-    }
-
-
-
-    function handleLoadUsers() {
-      AdminUserApi.loadAdminUsers(vm.condition, vm.page)
-        .success(res => {
-          vm.users = res.docs;
-          vm.totalPage = createArrayFromRange(res.pages);
-          vm.totalUser = res.total;
-        })
-        .error(err => {
-          alert(err.message);
-        });
-    }
-
-    vm.handleChangePage = page => {
-      vm.page = page;
-      handleLoadUsers();
-    };
-    vm.handleSearch = () => {
-      vm.page = 1;
-      handleLoadUsers();
-    };
-    vm.handleClearCondition = () => {
-      vm.condition = {};
-      vm.page = 1;
-      handleLoadUsers();
-    };
-    function createArrayFromRange(range) {
-      var array = [];
-      for (var i = 1; i <= range; i++) {
-        array.push(i);
-      }
-      return array;
     }
   }
 }());
